e8f7da7866e13512f7e1cf026265562ad2e2b6ed
psadalja <parthadalja29@gmail.com>
2021-08-11 15:04:26 +0530

    for loop to print data

--- script/script.js
+++ script/script.js
@@ -7,22 +7,40 @@ function getData() {
 }
 
 function displayNewsData(articles) {
-  // console.log(articles[0]);
-  const cardDivElement = document.createElement("div");
-  cardDivElement.className = "news-card";
+  console.log(articles[0]);
+  articles.forEach((element) => {
+    const cardDivElement = document.createElement("div");
+    cardDivElement.className = "news-card";
 
-  const imageDivElement = document.createElement("img");
-  imageDivElement.className = "news-image";
+    const imageDivElement = document.createElement("img");
+    imageDivElement.className = "news-image";
 
-  const publicationDivElement = document.createElement("div");
-  publicationDivElement.className = "publication-info";
+    const publicationDivElement = document.createElement("div");
+    publicationDivElement.className = "publication-info";
 
-  const discriptionDivElement = document.createElement("div");
-  discriptionDivElement.className = "news-discription";
+    const publicationPElement = document.createElement("p");
+    publicationPElement.id = "publication-name";
 
-  const fullNewsDivElement = document.createElement("div");
-  fullNewsDivElement.className = "full-news";
+    const publishedPElement = document.createElement("p");
+    publishedPElement.id = "published-date";
+
+    const discriptionDivElement = document.createElement("div");
+    discriptionDivElement.className = "news-discription";
+
+    const titleH1Element = document.createElement("h1");
+    titleH1Element.className = "news-headline";
+
+    const fullNewsDivElement = document.createElement("div");
+    fullNewsDivElement.className = "full-news";
+
+    const fullNewsButtonElement = document.createElement("button");
+    fullNewsButtonElement.className = "btn-full-news";
+
+    imageDivElement.src = articles[0].urtToImg;
+  });
 }
+
+// displayNewsData(data.articles);
 
 // description: "If you’re an Apple devotee, there’s a good chance the company’s Messages and FaceTime services are two of the key features that keep you locked into the Cupertino ecosystem. But now, Apple has opened the floodgates just a crack, allowing us to invite anyone t…"
 // publishedAt: "2021-08-10T14:00:00Z"
